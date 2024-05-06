25e69eb9a329e918d1cf16eaf59078d148877ed7
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-05-06 11:46:27 +0200

    Make small enemies jump

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -14,7 +14,6 @@ const ENEMY_TARGET_MATERIAL: Material = preload("res://scenes/enemies/enemy_targ
 		hit.emit()
 		
 		if health <= 0:
-			print(health)
 			queue_free()
 
 
--- scripts/enemies/enemy_small.gd
+++ scripts/enemies/enemy_small.gd
@@ -2,6 +2,7 @@ extends Enemy
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+#var last_tried_jump_velocity: float = 0
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +14,21 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= gravity * delta
 	else:
 		velocity += (GameManager.player.position - position).normalized() * speed * delta
+	
+		if is_on_wall():
+			velocity = (GameManager.player.position - position).normalized() * speed * 25 * delta
+			velocity.y = 7
+			
+			# Dynamic jump velocity. A little buggy. Uncomment when needed.
+			#if velocity.y > 0 and last_tried_jump_velocity:
+				#velocity.y = last_tried_jump_velocity
+			#else:
+				#if not last_tried_jump_velocity:
+					#velocity.y = 7
+				#else:
+					#velocity.y = last_tried_jump_velocity * 1.5
+				#
+				#last_tried_jump_velocity = velocity.y
 	
 	if position.distance_to(GameManager.player.position) > 100:
 		velocity = (GameManager.player.position - position).normalized() * speed * 100 * delta
